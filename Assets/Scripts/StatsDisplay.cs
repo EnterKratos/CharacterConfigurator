6beb0da1ee20626a1e21ec6a15a2779b6bed6b1c
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-12-10 17:41:41 +0000

    Update labels when character config changes

---
+++ Assets/Scripts/StatsDisplay.cs
@@ -0,0 +1,36 @@
+﻿using CharacterConfigurator.ScriptableObjects;
+using TMPro;
+using UnityEngine;
+
+namespace CharacterConfigurator
+{
+    public class StatsDisplay : MonoBehaviour
+    {
+        public CharacterData characterData;
+
+        public TextMeshProUGUI modelLabel;
+        public TextMeshProUGUI modelPriceLabel;
+
+        public TextMeshProUGUI weaponLabel;
+        public TextMeshProUGUI weaponPriceLabel;
+
+        public TextMeshProUGUI colourPriceLabel;
+
+        public TextMeshProUGUI totalPriceLabel;
+
+        private void OnEnable()
+        {
+            OnCharacterConfigChanged();
+        }
+
+        public void OnCharacterConfigChanged()
+        {
+            modelLabel.text = characterData.model.name;
+            modelPriceLabel.text = Helpers.FormatCurrencyValue(characterData.model.price);
+            weaponLabel.text = characterData.weapon.name;
+            weaponPriceLabel.text = Helpers.FormatCurrencyValue(characterData.weapon.price);
+            colourPriceLabel.text = Helpers.FormatCurrencyValue(characterData.colour.price);
+            totalPriceLabel.text = Helpers.FormatCurrencyValue(characterData.Total);
+        }
+    }
+}
