27518d581f390088cc3485c8acb18791054221db
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-12-10 17:13:19 +0000

    Track current character configuration

--- Assets/Scripts/ScriptableObjects/CharacterData.cs
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -9,6 +9,36 @@ namespace CharacterConfigurator.ScriptableObjects
         public WeaponData weapon;
         public ColourData colour;
 
+        [SerializeField]
+        [HideInInspector]
+        private ModelData _model;
+
+        [SerializeField]
+        [HideInInspector]
+        private WeaponData _weapon;
+
+        [SerializeField]
+        [HideInInspector]
+        private ColourData _colour;
+
         public double Total => model.price + weapon.price + colour.price;
+
+        public void Set(ModelData input) => model = input;
+        public void Set(WeaponData input) => weapon = input;
+        public void Set(ColourData input) => colour = input;
+
+        private void OnEnable()
+        {
+            _model = model;
+            _weapon = weapon;
+            _colour = colour;
+        }
+
+        private void OnDisable()
+        {
+            model = _model;
+            weapon = _weapon;
+            colour = _colour;
+        }
     }
 }
