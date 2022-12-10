216ed689c073f223d7b29a18502c7a8ee8c1bae3
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-12-10 22:49:22 +0000

    Update colour swatch when config changes

---
+++ Assets/Scripts/ColourSwatch.cs
@@ -0,0 +1,22 @@
+﻿using CharacterConfigurator.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CharacterConfigurator
+{
+    public class ColourSwatch : MonoBehaviour
+    {
+        public CharacterData characterData;
+        public Image image;
+
+        public void OnCharacterChanged()
+        {
+            image.color = characterData.colour.colour;
+        }
+
+        private void OnEnable()
+        {
+            OnCharacterChanged();
+        }
+    }
+}
