3bd7e809ae24d545b73ce7631f6228ebb9982321
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-12-11 18:56:28 +0000

    Add colour images to colour buttons

---
+++ Assets/Scripts/ColourUpdater.cs
@@ -0,0 +1,22 @@
+﻿using CharacterConfigurator.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CharacterConfigurator
+{
+    public class ColourUpdater : MonoBehaviour
+    {
+        public Image image;
+        public ColourData colourData;
+
+        public void Update()
+        {
+            image.color = colourData.colour;
+        }
+
+        private void OnEnable()
+        {
+            Update();
+        }
+    }
+}
