0273df0ba14c0521efee4a7d9f24b77c63f8699a
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2022-12-11 15:13:45 +0000

    Add button to change camera view

---
+++ Assets/Scripts/CameraPriorityUpdater.cs
@@ -0,0 +1,22 @@
+﻿using CharacterConfigurator.ScriptableObjects;
+using Cinemachine;
+using UnityEngine;
+
+namespace CharacterConfigurator
+{
+    public class CameraPriorityUpdater : MonoBehaviour
+    {
+        public CinemachineVirtualCamera virtualCamera;
+        public IntVariable cameraPriority;
+
+        public void Update()
+        {
+            virtualCamera.Priority = cameraPriority.value;
+        }
+
+        private void OnEnable()
+        {
+            Update();
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleIntVariable.cs
@@ -0,0 +1,22 @@
+﻿using CharacterConfigurator.ScriptableObjects;
+using UnityEngine;
+
+namespace CharacterConfigurator
+{
+    public class ToggleIntVariable : MonoBehaviour
+    {
+        public IntVariable intVariable;
+        public int value1;
+        public int value2;
+
+        public void Toggle()
+        {
+            intVariable.value = intVariable.value == value1 ? value2 : value1;
+        }
+
+        private void OnEnable()
+        {
+            intVariable.value = value1;
+        }
+    }
+}
